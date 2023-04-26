f8bba5a21d4fae0e4708e49c79e498bddbfd5839
oda <ooddaa@gmail.com>
2023-04-26 11:23:52 +0100

    [p371] DemographicsLive.Show.details component

---
+++ lib/help_web/live/demographic_live/show.ex
@@ -0,0 +1,21 @@
+defmodule HelpWeb.DemographicLive.Show do
+  use Phoenix.Component
+  use Phoenix.HTML
+  alias Help.Survey.Demographic
+
+  attr :demographic, Demographic, required: true
+
+  def details(assigns) do
+    ~H"""
+    <div>
+      <h2 class={["font-medium text-2xl"]}>
+        Demographics <%= raw("&#x2713;") %>
+      </h2>
+      <ul>
+        <li>Gender: <%= @demographic.gender %></li>
+        <li>Year of birth: <%= @demographic.year_of_birth %></li>
+      </ul>
+    </div>
+    """
+  end
+end
--- lib/help_web/live/survey_live.ex
+++ lib/help_web/live/survey_live.ex
@@ -1,8 +1,17 @@
 defmodule HelpWeb.SurveyLive do
   use HelpWeb, :live_view
   alias __MODULE__.Component
+  alias Help.Survey
+  alias HelpWeb.DemographicLive
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     socket
+     |> assign_demoraphic()}
+  end
+
+  defp assign_demoraphic(%{assigns: %{current_user: current_user}} = socket) do
+    socket
+    |> assign(:demographic, Survey.get_demographic_by_user(current_user))
   end
 end
