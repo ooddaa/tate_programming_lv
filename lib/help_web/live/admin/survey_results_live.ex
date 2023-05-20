0be5729d8410bfb3a481e818d07dfbc17b0f939d
oda <ooddaa@gmail.com>
2023-05-20 11:29:29 +0100

    [p509] render chart

--- lib/help_web/live/admin/survey_results_live.ex
+++ lib/help_web/live/admin/survey_results_live.ex
@@ -1,6 +1,7 @@
 defmodule HelpWeb.Admin.SurveyResultsLive do
   use HelpWeb, :live_component
   alias Help.Catalog
+  alias Contex.Plot
 
   def mount(socket) do
     {:ok, socket}
@@ -13,28 +14,30 @@ defmodule HelpWeb.Admin.SurveyResultsLive do
       |> assign(assigns)
       |> assign_products_with_average_ratings()
       |> assign_dataset()
+      |> assign_chart()
+      |> assign_chart_svg()
       #  |> IO.inspect()
     }
   end
 
   @spec assign_products_with_average_ratings(map) :: map
-  def assign_products_with_average_ratings(socket) do
+  defp assign_products_with_average_ratings(socket) do
     socket
     |> assign(:products_with_average_ratings, Catalog.product_with_average_ratings())
   end
 
-  def assign_dataset(
-        %{
-          assigns: %{
-            products_with_average_ratings: products_with_average_ratings
-          }
-        } = socket
-      ) do
+  defp assign_dataset(
+         %{
+           assigns: %{
+             products_with_average_ratings: products_with_average_ratings
+           }
+         } = socket
+       ) do
     socket
     |> assign(:dataset, make_bar_chart_dataset(products_with_average_ratings))
   end
 
-  def make_bar_chart_dataset(data) do
+  defp make_bar_chart_dataset(data) do
     Contex.Dataset.new(data)
     # |> IO.inspect(label: "dataset: ")
     # dataset: : %Contex.Dataset{
@@ -43,4 +46,72 @@ defmodule HelpWeb.Admin.SurveyResultsLive do
     #   title: nil
     # }
   end
+
+  defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket
+    |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
+    # |> IO.inspect(label: "chart: ")
+    # %Contex.BarChart{
+    #   dataset: %Contex.Dataset{
+    #     headers: nil,
+    #     data: [{"test product", 5.0}, {"adasf", 4.0}],
+    #     title: nil
+    #   },
+    #   mapping: %Contex.Mapping{
+    #     column_map: %{category_col: 0, value_cols: [1]},
+    #     accessors: %{
+    #       category_col: #Function<11.75387700/1 in Contex.Dataset.value_fn/2>,
+    #       value_cols: [#Function<11.75387700/1 in Contex.Dataset.value_fn/2>]
+    #     },
+    #     expected_mappings: [category_col: :exactly_one, value_cols: :one_or_more],
+    #     dataset: %Contex.Dataset{
+    #       headers: nil,
+    #       data: [{"test product", 5.0}, {"adasf", 4.0}],
+    #       title: nil
+    #     }
+    #   },
+    #   options: [
+    #     type: :stacked,
+    #     orientation: :vertical,
+    #     axis_label_rotation: :auto,
+    #     custom_value_scale: nil,
+    #     custom_value_formatter: nil,
+    #     width: 100,
+    #     height: 100,
+    #     padding: 2,
+    #     data_labels: true,
+    #     colour_palette: :default,
+    #     phx_event_handler: nil,
+    #     phx_event_target: nil,
+    #     select_item: nil
+    #   ],
+    #   category_scale: nil,
+    #   value_scale: nil,
+    #   series_fill_colours: nil,
+    #   phx_event_handler: nil,
+    #   value_range: nil,
+    #   select_item: nil
+    # }
+  end
+
+  defp assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:chart_svg, render_bar_chart(chart))
+  end
+
+  defp render_bar_chart(chart) do
+    Plot.new(500, 400, chart)
+    |> Plot.titles(title(), subtitle())
+    |> Plot.axis_labels(x_axis(), y_axis())
+    |> Plot.to_svg()
+  end
+
+  defp title, do: "Product Ratings"
+  defp subtitle, do: "average star ratings per product"
+  defp x_axis, do: "products"
+  defp y_axis, do: "stars"
 end
