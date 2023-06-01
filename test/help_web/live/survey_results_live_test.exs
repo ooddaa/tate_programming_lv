2e7e604b28815a736b1b4b5c2b4cfe0908460f6a
oda <ooddaa@gmail.com>
2023-06-01 19:46:49 +0200

    [p611] integration test ok
    CHANGES:

--- test/help_web/live/survey_results_live_test.exs
+++ test/help_web/live/survey_results_live_test.exs
@@ -20,12 +20,12 @@ defmodule HelpWeb.SurveyResultsLiveTest do
     email: "email2@email.com"
   }
   @create_demographic_attrs %{
-    gender: "male",
-    year_of_birth: DateTime.utc_now().year - 25
+    gender: "female",
+    year_of_birth: DateTime.utc_now().year - 15
   }
   @create_demographic_attrs2 %{
-    gender: "female",
-    year_of_birth: DateTime.utc_now().year - 18
+    gender: "male",
+    year_of_birth: DateTime.utc_now().year - 30
   }
 
   defp product_fixture do
@@ -88,22 +88,51 @@ defmodule HelpWeb.SurveyResultsLiveTest do
       # :register_and_log_in_user
     ]
 
-    # setup do
-    # setup %{user: user} = socket do
-    #   create_demographic(user)
-    #   user2 = user_fixture(@create_user_attrs2)
-    #   demographic_fixture(user2, @create_demographic_attrs2)
-    #   [user2: user2]
-    # end
+    setup %{user: user} = socket do
+      create_demographic(user)
+      user2 = user_fixture(@create_user_attrs2)
+      demographic_fixture(user2, @create_demographic_attrs2)
+      [user2: user2]
+    end
 
     test "no ratings", %{socket: socket} do
-      socket =
-        socket
-        |> SurveyResultsLive.assign_gender_filter()
-        |> SurveyResultsLive.assign_age_group_filter()
-        |> SurveyResultsLive.assign_products_with_average_ratings()
+      socket
+      |> SurveyResultsLive.assign_gender_filter()
+      |> SurveyResultsLive.assign_age_group_filter()
+      |> SurveyResultsLive.assign_products_with_average_ratings()
+      |> assert_keys(:products_with_average_ratings, [{"product_name", 0}])
+    end
 
-      assert socket.assigns.products_with_average_ratings == [{"product_name", 0}]
+    test "ratings are filtered by age group", %{
+      socket: socket,
+      user: user,
+      user2: user2,
+      product: product
+    } do
+      create_rating(2, user, product)
+      create_rating(5, user2, product)
+
+      # sets to "all" by default
+      socket
+      |> SurveyResultsLive.assign_age_group_filter()
+      |> assert_keys(:age_group_filter, "all")
+      # if filter has been set, does not reset to "all" by default
+      |> update_socket(:age_group_filter, "under 18")
+      |> SurveyResultsLive.assign_age_group_filter()
+      |> assert_keys(:age_group_filter, "under 18")
+      # check avg ratings for under 18s - which is user with 2
+      |> SurveyResultsLive.assign_gender_filter()
+      |> SurveyResultsLive.assign_products_with_average_ratings()
+      |> assert_keys(:products_with_average_ratings, [{"product_name", 2.0}])
+    end
+
+    defp update_socket(socket, key, value) do
+      %{socket | assigns: Map.merge(socket.assigns, Map.new([{key, value}]))}
+    end
+
+    defp assert_keys(socket, key, value) do
+      assert socket.assigns[key] == value
+      socket
     end
   end
 end
